8551487b81a0332335a4ca77d45d5968b090231c
shaik samiulla <sksamiulla1111@gmail.com>
2020-07-29 12:10:53 +0530

    added all till posts

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const homeController=require('../controllers/home_controller');
 router.get('/',homeController.home);
 router.use('/users',require('./user'));
 console.log('router loaded..');
+router.use('/posts',require('./posts'));
 
 module.exports=router;
