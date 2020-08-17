7f5456b4011a4554834d53f1ab33a1df9d16f49a
Shaik Samiulla <44118231+samiulla1111@users.noreply.github.com>
2020-08-17 14:36:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
-
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
 
 const { setAuthenticatedUser } = require('./config/passport-local-strategy');
 const { Store } = require('express-session');
